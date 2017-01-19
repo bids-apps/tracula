b557e067646e85173c82958d9ad433016a054484
fliem <franz.liem@gmail.com>
2017-01-19 11:04:53 +0100

    helptext ud

--- run.py
+++ run.py
@@ -236,7 +236,8 @@ def calculate_tmi(df):
 
 __version__ = open('/version').read()
 
-parser = argparse.ArgumentParser(description='BIDS App for Tracula processing stream. '
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                 description='BIDS App for  Tracula processing stream. '
                                              'https://surfer.nmr.mgh.harvard.edu/fswiki/Tracula')
 parser.add_argument('bids_dir', help='The directory with the input dataset '
                                      'formatted according to the BIDS standard.')
@@ -245,30 +246,28 @@ parser.add_argument('output_dir', help='The directory where the output files '
                                        'this folder should be prepopulated with the results of the'
                                        'participant level analysis.')
 parser.add_argument('analysis_level', help='Level of the analysis that will be performed. '
-                                           'participant: reconstructs paths (trac-all -prep, -bedp and -path), '
-                                           'group1: collects motion stats in one file, '
-                                           'group2: collects single subject overall path stats in one file.',
+                                           '"participant": reconstructs paths (trac-all -prep, -bedp and -path), '
+                                           '"group1": collects motion stats in one file, '
+                                           '"group2": collects single subject overall path stats in one file.',
                     choices=['participant', 'group1', 'group2'])
 parser.add_argument('--license_key', help='FreeSurfer license key - letters and numbers after "*" in the email '
                                           'you received after registration. To register (for free) visit '
                                           'https://surfer.nmr.mgh.harvard.edu/registration.html', required=True)
-parser.add_argument('--participant_label', help='The label of the participant that should be analyzed. The label '
-                                                'corresponds to sub-<participant_label> from the BIDS spec '
-                                                '(so it does not include "sub-"). If this parameter is not '
-                                                'provided all subjects should be analyzed. Multiple '
-                                                'participants can be specified with a space separated list.',
-                    nargs="+")
+parser.add_argument('--participant_label',
+                    help='The label of the participant that should be analyzed. The label '
+                         'corresponds to sub-<participant_label> from the BIDS spec '
+                         '(so it does not include "sub-"). If this parameter is not '
+                         'provided all subjects should be analyzed. Multiple '
+                         'participants can be specified with a space separated list.', nargs="+")
 parser.add_argument('--freesurfer_dir', help='The directory with the freesurfer data. If not specified,'
                                              'output_dir is assumed to be populated with freesurfer data')
-parser.add_argument('--stages', help="Participant-level trac-all stages to run. "
-                                     "'all' will run 'prep', 'bedp' and 'path'",
+parser.add_argument('--stages', help='Participant-level trac-all stages to run. '
+                                     '"all" will run "prep", "bedp" and "path". ',
                     choices=["prep", "bedp", "path", "all"], default=["all"], nargs="+")
-parser.add_argument('-v', '--version', action='version', version='Tracula BIDS-App version {}'.format(
-    __version__))
+parser.add_argument('-v', '--version', action='version',
+                    version='Tracula BIDS-App version {}'.format(__version__))
 
 # parser.add_argument('--n_cpus', help='Number of CPUs/cores available to use.', default=1, type=int)
-# parser.add_argument('--stages', help='Tracula single subject stages to run. all runs prep, bedp, and path.',
-#                     choices=["prep", "bedp", "path", "all"], default=["all"], nargs="+")
 
 args = parser.parse_args()
 
