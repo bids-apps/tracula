e8b455f49f9587f9ac59736fb0a7293b7bb6ebb2
fliem <franz.liem@gmail.com>
2017-08-08 06:44:37 +0200

    cleanup [skip ci]

--- full_tests/run_full_tests_v6.0.0-3.sh
+++ full_tests/run_full_tests_v6.0.0-3.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
-# run in /data.nfs/ds114/tracula_full_tests
+# cd /data.nfs/ds114/tracula_full_tests
+# screen -L bash run_full_tests_....sh ds114_test1 and
+# screen -L bash run_full_tests_....sh ds114_test2
+
+ds_name=$1
+echo Running ${ds_name}
 
 tracula_version=v6.0.0-3
-wd=$PWD/${tracula_version}
-data_dir=${wd}/data/
-out_root_dir=${wd}/out/
+wd=/data.nfs/ds114/tracula_full_tests/${tracula_version}
+
+data_dir=${wd}/data
+out_root_dir=${wd}/out
 
 mkdir -p $data_dir
 mkdir -p $out_root_dir
@@ -17,78 +23,44 @@ if [[ ! -d ${data_dir}/ds114_test2_freesurfer ]]; then wget -c -O ${data_dir}/ds
 if [[ -e ${data_dir}/*.tar ]]; then rm -r ${data_dir}/*.tar; fi
 
 
-#### ds114_test1
-in_dir=${data_dir}/ds114_test1
-fs_dir=${data_dir}/ds114_test1_freesurfer
-out_dir=${out_root_dir}/ds114_test1
+
+
+in_dir=${data_dir}/${ds_name}
+in_dir=${data_dir}/${ds_name}_freesurfer_precomp_v6.0.0
+out_dir=${out_root_dir}/${ds_name}
 mkdir -p $out_dir
+chmod -R 777 $out_dir
 cd $out_dir
 
-screen -L docker run --rm -ti \
+docker run --rm -ti \
 -v ${in_dir}:/data/in \
 -v ${fs_dir}:/data/fs \
 -v ${out_dir}:/data/out \
 bids/tracula:${tracula_version} \
 /data/in /data/out participant \
---freesurfer_dir /data/fs \
---license_key xxx
+--license_key xxx \
+--freesurfer_dir /data/fs
 
 
-screen -L docker run --rm -ti \
+docker run --rm -ti \
 -v ${in_dir}:/data/in \
 -v ${fs_dir}:/data/fs \
 -v ${out_dir}:/data/out \
 bids/tracula:${tracula_version} \
 /data/in /data/out group1 \
---freesurfer_dir /data/fs \
---license_key xxx
+--license_key xxx \
+--freesurfer_dir /data/fs
 
 
-screen -L docker run --rm -ti \
+
+docker run --rm -ti \
 -v ${in_dir}:/data/in \
 -v ${fs_dir}:/data/fs \
 -v ${out_dir}:/data/out \
 bids/tracula:${tracula_version} \
 /data/in /data/out group2 \
---freesurfer_dir /data/fs \
---license_key xxx
+--license_key xxx \
+--freesurfer_dir /data/fs
 
-tar -zcvf ${wd}/results_tracula_${tracula_version}_ds114_test1.tar.gz ${out_dir}
+tar -zcvf ${wd}/results_tracula_${tracula_version}_${ds_name}.tar.gz ${out_dir}
 
-
-
-#### ds114_test2
-in_dir=${data_dir}/ds114_test2
-fs_dir=${data_dir}/ds114_test2_freesurfer
-out_dir=${out_root_dir}/ds114_test2
-mkdir -p $out_dir
-cd $out_dir
-
-screen -L docker run --rm -ti \
--v ${in_dir}:/data/in \
--v ${fs_dir}:/data/fs \
--v ${out_dir}:/data/out \
-bids/tracula:${tracula_version} \
-/data/in /data/out participant \
---freesurfer_dir /data/fs \
---license_key xxx
-
-screen -L docker run --rm -ti \
--v ${in_dir}:/data/in \
--v ${fs_dir}:/data/fs \
--v ${out_dir}:/data/out \
-bids/tracula:${tracula_version} \
-/data/in /data/out group1 \
---freesurfer_dir /data/fs \
---license_key xxx
-
-screen -L docker run --rm -ti \
--v ${in_dir}:/data/in \
--v ${fs_dir}:/data/fs \
--v ${out_dir}:/data/out \
-bids/tracula:${tracula_version} \
-/data/in /data/out group2 \
---freesurfer_dir /data/fs \
---license_key xxx
-
-tar -zcvf ${wd}/results_tracula_${tracula_version}_ds114_test2.tar.gz ${out_dir}
